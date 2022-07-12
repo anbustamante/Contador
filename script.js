ba6d63585c9006c7e11ac779c93fd9f4c5a82754
Andy <andy00b@gmail.com>
2022-07-12 15:45:47 -0300

    boton de reset funcionando

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ let contadorL = document.getElementById("plocal");
 let sumaL = 0;
 let contadorV = document.getElementById("pvis");
 let sumaV = 0;
+let banderaL = 0;
+let banderaV = 0;
 
 function incrementL(puntos){
     sumaL += puntos;
     contadorL.innerText = sumaL;
+    banderaL = 1;
+    banderaV = 0;
 }
 function incrementV(puntos){
     sumaV += puntos;
     contadorV.innerText = sumaV;
+    banderaL = 0;
+    banderaV = 1;
+}
+
+function newgame(){
+    sumaL = 0;
+    sumaV = 0;
+    contadorV.innerText = sumaL;
+    contadorL.innerText = sumaV;
 }
